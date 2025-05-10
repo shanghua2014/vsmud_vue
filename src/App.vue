7639d191f91e9a6c0ad3881644eb5d290b5595ec
shanghua2014 <shanghua_021@qq.com>
2025-05-10 14:39:00 +0800

    添加按钮

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,44 @@
 <template>
-    <Terminal v-if="showTerminal" />
-    <Mudlist v-if="!showTerminal" :mudlist="mudlist" @card-clicked="receive.cardClicked" />
+    <div class="app-container">
+        <MenuButton />
+        <Terminal v-if="showTerminal" />
+        <Mudlist v-if="!showTerminal" :mudlist="mudlist" @card-clicked="receive.cardClicked" />
+    </div>
 </template>
 
 <script lang="ts" setup>
-import Terminal from './components/Terminal.vue'
-import Mudlist from './components/MudList.vue'
-import { onMounted, ref, onUnmounted } from 'vue'
-import { Base } from './global'
+import Terminal from './components/Terminal.vue';
+import Mudlist from './components/MudList.vue';
+import MenuButton from './components/MenuButton.vue'; // 引入 MenuButton 组件
+import { onMounted, ref, onUnmounted } from 'vue';
+import { Base } from './global';
 
-const showTerminal = ref(false)
-const mudlist = ref<any>({})
+const showTerminal = ref(true);
+const mudlist = ref<any>({});
 
 // =======================
 //    接收子组件的消息
 // =======================
 const receive = {
     cardClicked: () => {
-        showTerminal.value = true
-    }
-}
+        showTerminal.value = true;
+    },
+};
 
 // 接收来自vscode扩展的消息
 onMounted(() => {
-    new Base().postMessage({ type: 'getAccount', content: '' })
-})
+    new Base().postMessage({ type: 'getAccount', content: '' });
+});
 
 // 组件卸载时断开观察器
 onUnmounted(() => {
-    window.removeEventListener('message', () => {})
-})
+    window.removeEventListener('message', () => {});
+});
 </script>
+
+<style lang="scss" scoped>
+.app-container {
+    position: relative;
+    height: 100vh;
+}
+</style>
---
+++ src/components/MenuButton.vue
@@ -0,0 +1,62 @@
+<template>
+    <el-menu :default-active="activeIndex" class="el-menu-demo pa" mode="horizontal" :ellipsis="false" @select="handleSelect" :collapse="true">
+        <el-sub-menu index="2">
+            <template #title
+                ><el-icon><Document /></el-icon
+            ></template>
+            <el-sub-menu index="1-1">
+                <template #title>脚本1</template>
+                <el-menu-item index="1-1-1">重载1</el-menu-item>
+            </el-sub-menu>
+            <el-sub-menu index="2-4">
+                <template #title>脚本</template>
+                <el-menu-item index="2-4-1">重载</el-menu-item>
+            </el-sub-menu>
+        </el-sub-menu>
+    </el-menu>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+import { ArrowUp, ArrowDown, Document } from '@element-plus/icons-vue' // 导入图标组件
+
+const activeIndex = ref('1')
+const handleSelect = (key: string, keyPath: string[]) => {
+    console.log(key, keyPath)
+}
+</script>
+
+<style lang="scss">
+ul.el-menu-demo {
+    height: 40px;
+    width: 90px;
+    bottom: 34px;
+    right: 2px;
+    background: #1f1f1f;
+    border: 1px solid var(--el-menu-border-color);
+    justify-content: center;
+    z-index: 1;
+}
+ul.el-menu--popup {
+    padding: 0;
+    min-width: auto;
+}
+.el-menu--horizontal .el-menu .el-menu-item,
+.el-menu--horizontal .el-menu .el-sub-menu__title {
+    justify-content: space-around;
+}
+div.el-menu--horizontal .el-menu .el-sub-menu__title {
+    padding: 0;
+    width: 90px;
+}
+div.el-popper.is-pure:first-child {
+    inset: auto 0 79px auto !important;
+}
+.el-menu--horizontal > .el-menu-item:nth-child(1) {
+    margin-right: auto;
+}
+/* 隐藏默认图标 */
+.el-menu--horizontal .el-sub-menu__title .el-sub-menu__icon-arrow {
+    display: none;
+}
+</style>
